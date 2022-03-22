c74cd17b3c3d993de6e213d220bde6cd95e96e42
Crespone <crework96@gmail.com>
2022-03-22 21:15:04 +0100

    first commit

---
+++ gitHubCall.bat
@@ -0,0 +1,8 @@
+cd C:\Users\New\Documents\JAVA_COURSE_UDEMY
+git init 
+git status
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Crengineer/MyJavaExercises.git
+git push -u origin main
