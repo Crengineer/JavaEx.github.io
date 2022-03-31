b0562c437e198a334c6dbc84e430cb65db9d346a
Crengineer <crespi.marco96@gmail.com>
2022-03-31 20:39:35 +0200

    YOLO

--- Java_programs/NewInheritance/src/com/company/SportCar.java
+++ Java_programs/NewInheritance/src/com/company/SportCar.java
@@ -2,57 +2,135 @@ package com.company;
 
 public class SportCar extends Car{
 
-    public SportCar(String name, String model, int wheelNumber, int horsePower, String driverLicence, int speed, String currentDirection, String status) {
-        super(name, model, wheelNumber, horsePower, driverLicence, speed, currentDirection, status);
+    public SportCar(String name, String model, int wheelNumber, int horsePower, String driverLicence, int speed, String currentDirection, String status, int gear) {
+        super(name, model, wheelNumber, horsePower, driverLicence, speed, currentDirection, status, gear);
     }
 
-    @Override
     public String getName() {
         return super.getName();
     }
 
-    @Override
     public void setName(String name) {
         super.setName(name);
     }
 
-    @Override
     public String getModel() {
         return super.getModel();
     }
 
-    @Override
     public void setModel(String model) {
         super.setModel(model);
     }
 
-    @Override
     public int getWheelNumber() {
-        return 4;
+        return super.getWheelNumber();
     }
 
-    @Override
     public void setWheelNumber(int wheelNumber) {
-        super.setWheelNumber(wheelNumber=4);
+        super.setWheelNumber(wheelNumber);
     }
 
-    @Override
     public int getHorsePower() {
         return super.getHorsePower();
     }
 
-    @Override
     public void setHorsePower(int horsePower) {
         super.setHorsePower(horsePower);
     }
 
-    @Override
     public String getDriverLicence() {
-        return "S";
+        return super.getDriverLicence();
+    }
+
+    public void setDriverLicence(String driverLicence) {
+        super.setDriverLicence(driverLicence);
+    }
+
+    public int getSpeed() {
+        return super.getSpeed();
+    }
+
+    public void setSpeed(int speed) {
+        super.setSpeed(speed);
+    }
+
+    public String getCurrentDirection() {
+        return super.getCurrentDirection();
+    }
+
+    public void setCurrentDirection(String currentDirection) {
+        super.setCurrentDirection(currentDirection);
+    }
+
+    public String getStatus() {
+        return super.getStatus();
+    }
+
+    public void setStatus(String status) {
+        super.setStatus(status);
+    }
+
+    public void steerRight() {
+        super.steerRight();
+    }
+
+    public int getGear() {
+        return super.getGear();
+    }
+
+    public void setGear(int gear) {
+        super.setGear(gear);
+    }
+
+    public void steerLeft() {
+        super.steerLeft();
+    }
+
+    public void switchOn() {
+        super.switchOn();
+    }
+
+    public void switchOff() {
+        super.switchOff();
+    }
+
+    public void stop() {
+        super.stop();
+    }
+
+    public void increaseSpeed(int rate) {
+        super.increaseSpeed(rate);
+    }
+
+    public void decreaseSpeed(int rate) {
+        super.decreaseSpeed(rate);
     }
 
     @Override
-    public void setDriverLicence(String driverLicence) {
-        super.setDriverLicence(driverLicence="S");
+    public void gearSetting() {
+        if(getSpeed() >= 0 && getSpeed() <=10){
+            setGear(1);
+            return;
+        }
+        if(getSpeed() > 10 && getSpeed() <=20){
+            setGear(2);
+            return;
+        }
+        if(getSpeed() > 20 && getSpeed() <=50){
+            setGear(3);
+            return;
+        }
+        if(getSpeed() > 50 && getSpeed() <=70){
+            setGear(4);
+            return;
+        }
+        if(getSpeed()>=70 && getSpeed()<=90){
+            setGear(5);
+            return;
+        }
+        if(getSpeed()>=90){
+            setGear(6);
+            return;
+        }
     }
 }
--- Java_programs/NewInheritance/src/com/company/Vehicle.java
+++ Java_programs/NewInheritance/src/com/company/Vehicle.java
@@ -67,8 +67,107 @@ public class Vehicle {
         this.driverLicence = driverLicence;
     }
 
+    //SPEED GET & SET
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    //CURRENT DIRECTION GET & SET
+
+    public String getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(String currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    //STATUS GET & SET
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     // METHODS
 
-    public void steerRight
+    //STEERING RIGHT AND LEFT
+
+    public void steerRight(){
+        if(getStatus().equals("Off")){
+            System.out.println("Cannot turn the vehicle, because is off.");
+            return;
+        }
+        if(getCurrentDirection().equals("Right")){
+            setCurrentDirection("Right");
+            return;
+        }
+        if(getCurrentDirection().equals("Straight")){
+            setCurrentDirection("Right");
+            return;
+        }
+        if(getCurrentDirection().equals("Left")){
+            setCurrentDirection("Straight");
+            return;
+        }
+    }
+
+    public void steerLeft(){
+        if(getStatus().equals("Off")){
+            System.out.println("Cannot turn the vehicle, because is off.");
+            return;
+        }
+        if(getCurrentDirection().equals("Right")){
+            setCurrentDirection("Straight");
+            return;
+        }
+        if(getCurrentDirection().equals("Straight")){
+            setCurrentDirection("Left");
+            return;
+        }
+        if(getCurrentDirection().equals("Left")){
+            setCurrentDirection("Left");
+            return;
+        }
+    }
+
+    //SWITCHES OF THE ENGINE
+
+    public void switchOn(){
+        if(getStatus().equals("On")){
+            System.out.println("The vehicle is already on.");
+            return;
+        }
+        if(getStatus().equals("Off")){
+            setStatus("On");
+            return;
+        }
+    }
+
+    public void switchOff(){
+        if(getStatus().equals("Off")){
+            System.out.println("The vehicle is already off.");
+            return;
+        }
+        if(getStatus().equals("On")){
+            setStatus("Off");
+            setSpeed(0);
+            return;
+        }
+    }
+
+    public void stop(){
+        setSpeed(0);
+        return;
+    }
+
 
 }
